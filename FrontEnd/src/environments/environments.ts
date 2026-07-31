09628485fe03b823c38d01a055bad2536393d11d
AlexTS21 <alejanafs28@gmail.com>
2026-07-31 13:38:13 -0600

    Angular dist added to deploy

--- FrontEnd/src/environments/environments.ts
+++ FrontEnd/src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // ── Apunta a tu API .NET local ──
   // Cambiar a la URL de producción al desplegar
-  apiUrl: 'http://localhost:5000/api',
+  apiUrl: 'https://crepv.onrender.com/api',
 };
